27d15f54ba086bab3beed4ac09164e2ac69f3aa8
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-03-26 18:22:34 +0100

    Fixes for the prerelease call
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- scripts/prerelease_calls_from_rerelease.bash
+++ scripts/prerelease_calls_from_rerelease.bash
@@ -3,23 +3,30 @@
 set -e
 
 DISTRO=${DISTRO:-noble}
+DRY_RUN=${DRY_RUN:-true}
 
 if [[ -z ${TOKEN} ]]; then
   echo "Provide a TOKEN for calling jobs"
   exit 1
 fi
 
+dry_run_str=""
+if $DRY_RUN; then
+  dry_run_str="--dry-run"
+fi
+
+
 PACKAGE=$(dpkg-parsechangelog --file "${DISTRO}/debian/changelog" -S Source)
-CANONICAL_NAME="sed 's/[[:digit:]]*$//' <<< ${PACKAGE}"
+CANONICAL_NAME="${PACKAGE%%[0-9]*}"
 VERSION=$(dpkg-parsechangelog --file "${DISTRO}/debian/changelog" -S Version \
   | sed 's:-.*::')
 REVISION=$(dpkg-parsechangelog --file "${DISTRO}/debian/changelog" -S Version \
   | sed 's:.*-::' | sed 's:~.*::')
  
-SOURCE_TARBALL_URI="https://osrf-distributions.s3.amazonaws.com/${CANONICAL_NAME}/releases/${PACKAGE}-${VERSION}.tar.bz2"
+SOURCE_TARBALL_URI="https://osrf-distributions.s3.amazonaws.com/${CANONICAL_NAME}/releases/${CANONICAL_NAME}-${VERSION}.tar.bz2"
 
 
-_RELEASEPY_DEBUG=1 ~/code/release-tools/release.py --dry-run \
+ ~/code/release-tools/release.py ${dry_run_str} \
   --release-repo-branch "$(git branch --show-current)" \
   --source-tarball-uri "${SOURCE_TARBALL_URI}" \
   --upload-to prerelease \
